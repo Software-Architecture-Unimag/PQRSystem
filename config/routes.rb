bea1af1a3d3bdc92d87ca7965b005604d2de3c50
Adalberto Avila <adalbertoavila1013@gmail.com>
2018-05-29 02:39:06 -0500

    Se agrego Departamentos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :departments
   resources :rols
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_073521) do
+ActiveRecord::Schema.define(version: 2018_05_29_073845) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rols", force: :cascade do |t|
     t.string "name"
