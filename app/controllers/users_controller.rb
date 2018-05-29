63c93bd1ed542db76e855dabfbc8f3bdf7b3a9e2
Adalberto Avila <adalbertoavila1013@gmail.com>
2018-05-29 05:28:39 -0500

    Nuevo Usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,28 @@ class UsersController < ApplicationController
   end
 
   def show
-     @user = User.find params[:id]
+     @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(
+  						first_name: params[:users][:first_name],
+  						last_name: params[:users][:last_name],
+  						email: params[:users][:email],
+  						phone_number: params[:users][:phone_number],
+  						birth_date: params[:users][:birth_date],
+  						password: params[:users][:password]
+
+  					)
+  	if @article.save 
+  		redirect_to @article
+  	else
+  		render :new
+  	end
+  end
+
 end
